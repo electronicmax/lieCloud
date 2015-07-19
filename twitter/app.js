5ac24bf5ace415cd59032d4bf3f426a71ca236c2
electronic Max <electronic@gmail.com>
2015-07-19 16:06:11 +0100

    finished the auth craziness

--- twitter/app.js
+++ twitter/app.js
@@ -1,117 +1,101 @@
 /* jshint undef: true, strict:false, trailing:false, unused:false, quotmark:false */
-/* global require, exports, console, process, module, L, angular, _, jQuery, FastClick, device, document, window, setTimeout, $, Keyboard, Backbone */
+/* global require, exports, console, process, module, L, angular, jQuery, FastClick, device, document, window, setTimeout, $, Keyboard, Backbone */
 
 var express = require('express'),
-  url = require('url');
-var Flutter = require('flutter');
-var session = require('express-session'),
-    _ = require('underscore'),
-    redis = require("redis"),
-    client = redis.createClient(),
-    bluebird = require('bluebird'),
-    rget = function(key) { 
-      return new Promise(function(accept, reject) {
-        client.get(key, function(err, res) {
-          if (res !== null) { return accept(res); }
-          reject(err);
-        });
-      });
-    },
-    consumerKey = 'VV6ZRqrHqezOxl0xbOuo2ytZE',
-    consumerSecret = 'hc6nbiaotdnvK3isC04EQLAeAAKLtelSn0uz03RqhzaLRBl2SJ',
-    twitter = require('twitter');
+	url = require('url'),
+	guid = require('guid'),
+	Flutter = require('flutter'),
+	session = require('express-session'),
+	_ = require('underscore'),
+	redis = require("redis"),
+	client = redis.createClient(),
+	bluebird = require('bluebird'),
+	rget = function(tokenkey, key) { 
 
-
-var flutter = new Flutter({
-  consumerKey: consumerKey,
-  consumerSecret: consumerSecret,
-  loginCallback: 'http://localhost:3000/twitter/callback',
-  authCallback: function(req, res, next) {
-    if (req.error) {
-      // Authentication failed, req.error contains details
-      return;
-    }
-    var accessToken = req.session.oauthAccessToken;
-    var secret = req.session.oauthAccessTokenSecret;
-    // Redirect user back to your app
-    console.log('saving oaRT::' + req.session.oauthRequestToken + 
-        ' oaRS::' + req.session.oauthRequestTokenSecret + 
-        ' oaAT::' + req.session.oauthAccessToken + 
-        ' oaATs::' +  req.session.oauthAccessTokenSecret
-    );
-
-    client.set('oauthRequestToken', req.session.oauthRequestToken, console.log);
-    client.set('oauthRequestTokenSecret', req.session.oauthRequestTokenSecret, console.log);
-    client.set('oauthAccessToken', req.session.oauthAccessToken, console.log);
-    client.set('oauthAccessTokenSecret', req.session.oauthAccessTokenSecret, console.log);
-    res.redirect('/done');
-  }
-});
+		return new Promise(function(accept, reject) {
+			client.get(tokenkey, function(err, res) {
+				if (res !== null) { 
+					try { 
+						var tokenbundle = JSON.parse(res);
+						return accept(tokenbundle[key]);
+					} catch(e) { 
+						console.error('error decoding');
+						return reject(e);
+					}
+				}
+				reject(err);
+			});
+		});
+	},
+	consumerKey = 'VV6ZRqrHqezOxl0xbOuo2ytZE',
+	consumerSecret = 'hc6nbiaotdnvK3isC04EQLAeAAKLtelSn0uz03RqhzaLRBl2SJ',
+	twitter = require('twitter'),
+	flutter = new Flutter({
+		consumerKey: consumerKey,
+		consumerSecret: consumerSecret,
+		loginCallback: 'http://localhost:3000/twitter/callback',
+		authCallback: function(req, res, next) {
+			if (req.error) {
+				// Authentication failed, req.error contains details
+				return;
+			}
+			var redis_key = 'token-' + guid.create(),
+				accessToken = req.session.oauthAccessToken,
+				secret = req.session.oauthAccessTokenSecret;
+			console.log('req.session ', req.session);
+			client.set(redis_key, 
+				JSON.stringify({
+					oauthRequestToken: req.session.oauthRequestToken,
+					oauthRequestTokenSecret: req.session.oauthRequestTokenSecret,
+					oauthAccessToken: req.session.oauthAccessToken,
+					oauthAccessTokenSecret: req.session.oauthAccessTokenSecret
+				}),
+				console.log);
+			res.redirect('/model.html?tokenkey='+redis_key);
+		}
+	});
 
 var app = express();
 app.use(session({secret:'keyboard cat'}));
 app.get('/twitter/connect', flutter.connect);
 app.get('/twitter/callback', flutter.auth);
-app.get('/done', function(req, res) { 
-  res.send(JSON.stringify({
-      oauthRequestToken: req.session.oauthRequestToken,
-      oauthRequestTokenSecret: req.session.oauthRequestTokenSecret,
-      oauthAccessToken: req.session.oauthAccessToken,
-      oauthAccessTokenSecret: req.session.oauthAccessTokenSecret
-  }));
-});
-var getClient = function() { 
-  return Promise.all([rget('oauthAccessToken'), rget('oauthAccessTokenSecret')]).then(function(values) { 
-    var accessToken = values[0], secret = values[1];
-    if (accessToken && secret) { 
-      return new twitter({ 
-        consumer_key: consumerKey, consumer_secret: consumerSecret, 
-        access_token_key: accessToken, access_token_secret: secret 
-      });
-    }
-    throw new Error("oops");
-  });
+
+var getClient = function(tokenkey) { 
+	return Promise.all([rget(tokenkey, 'oauthAccessToken'), rget(tokenkey, 'oauthAccessTokenSecret')]).then(function(values) { 
+		var accessToken = values[0], secret = values[1];
+		console.log('aT ', accessToken, ' secret ', secret);
+		if (accessToken && secret) { 
+			return new twitter({ 
+				consumer_key: consumerKey, consumer_secret: consumerSecret, 
+				access_token_key: accessToken, access_token_secret: secret 
+			});
+		}
+		throw new Error("oops");
+	});
 };
 
 app.get('/twitter/call', function(req,res) {
-  var path = req.query.path,
-    params = _(req.query).omit('path');
-  console.log('calling ', path, ' - params ', params);
-  getClient().then(function(t)  {
-     t.get(path, params, function(error, tweets, response){      
-        if(error) { 
-          console.log('error!', error); 
-          res.send(JSON.stringify({error:true, message:error && error.message || error && error[0] && error[0].message}));
-          return;
-        }
-        res.send(tweets);
-    });  
-  });
-});
+	var path = req.query.path,
+		tokenkey = req.query.tokenkey,
+		params = _(req.query).omit('path', 'tokenkey');
 
-app.get('/twitter/dms', function(req,res) {
-  getClient().then(function(t)  {
-    // t.get('favorites/list', function(error, tweets, response){
-     // t.get('statuses/user_timeline', function(error, tweets, response){
-     t.get('direct_messages', {count:200}, function(error, tweets, response){      
-      if(error) { 
-        console.log('error!', error); 
-        return res.send('error ' + error[0].message);
-        // throw error;
-      }
-      console.log(tweets);  // The favorites.
-      res.send(tweets);
-      console.log(response);  // Raw response object.
-    });  
-  });
+	getClient(tokenkey).then(function(t)  {
+		 t.get(path, params, function(error, tweets, response){      
+				if(error) { 
+					console.log('error!', error); 
+					res.send(JSON.stringify({error:true, message:error && error.message || error && error[0] && error[0].message}));
+					return;
+				}
+				res.send(tweets);
+		});  
+	});
 });
-
 app.get('/', function (req, res) { res.send('Hello World!'); });
 app.use(express.static('www'));
 
 var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+	var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Example app listening at http://%s:%s', host, port);
 });
--- twitter/www/js/lie.js
+++ twitter/www/js/lie.js
@@ -1,13 +1,29 @@
 /* jshint undef: true, strict:false, trailing:false, unused:false, quotmark:false */
 /* global require, exports, console, process, module, L, angular, _, jQuery, FastClick, device, document, window, setTimeout, $, Keyboard, Backbone */
 
-var tojson = function(x) { return JSON.stringify(x); };
+
+
+var tojson = function(x) { return JSON.stringify(x); },
+	getUrlParameter = function(sParam)	{
+	    var sPageURL = window.location.search.substring(1);
+	    var sURLVariables = sPageURL.split('&');
+	    for (var i = 0; i < sURLVariables.length; i++) 
+	    {
+	        var sParameterName = sURLVariables[i].split('=');
+	        if (sParameterName[0] == sParam) 
+	        {
+	            return sParameterName[1];
+	        }
+	    }
+	};
 
 angular.module('lie', [])
 	.controller('main', function($scope, $http, $timeout) { 
-		var sa = function(fn) { if (window) { window.setTimeout(function() { $scope.$apply(fn); }, 0); } };
+		var sa = function(fn) { if (window) { window.setTimeout(function() { $scope.$apply(fn); }, 0); } },
+			tokenkey = getUrlParameter('tokenkey');
+
 		var callTwitter = function(path, params) { 
-			var data = _({path:path}).extend(params);
+			var data = _({path:path, tokenkey:tokenkey}).extend(params);
 			console.log('data ', data);
 			return new Promise(function(acc,rej) { 
 				$.ajax({ url:'/twitter/call', method:'GET', data: data }).then(acc).fail(rej);
@@ -35,12 +51,12 @@ angular.module('lie', [])
 		};
 
 		var alls = [
-			callTwitter('direct_messages', {count:200}).then(function(results) {
-				sa(function() { results.map(function(x) { 
-					console.log(x);
-					incr(x.sender.screen_name); 
-				}); });
-			}),
+			// callTwitter('direct_messages', {count:200}).then(function(results) {
+			// 	sa(function() { results.map(function(x) { 
+			// 		console.log(x);
+			// 		incr(x.sender.screen_name); 
+			// 	}); });
+			// }),
 			callTwitter('statuses/user_timeline', {count:500}).then(function(results) {
 				console.info('user_timeline ', results);
 				sa(function() { 
