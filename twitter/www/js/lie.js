a8883db15387d1a49f09ec3bb99c51f80b168952
electronic <electronic@iMac.local>
2015-07-19 14:08:08 +0100

    progress

--- twitter/www/js/lie.js
+++ twitter/www/js/lie.js
@@ -6,19 +6,57 @@ var tojson = function(x) { return JSON.stringify(x); };
 angular.module('lie', [])
 	.controller('main', function($scope, $http, $timeout) { 
 		var sa = function(fn) { if (window) { window.setTimeout(function() { $scope.$apply(fn); }, 0); } };
+		var callTwitter = function(path, params) { 
+			var data = _({path:path}).extend(params);
+			console.log('data ', data);
+			return new Promise(function(acc,rej) { 
+				$.ajax({ url:'/twitter/call', method:'GET', data: data }).then(acc).fail(rej);
+			});
+		};
+
 		$scope.method = 'direct_messages';
 		$scope.params = tojson({count:200});
+
 		$scope.call = function(c) { 
-			var data = _({ path : $scope.method }).extend(JSON.parse($scope.params));
-			console.log('data ', data);
-			$.ajax({ url:'/twitter/call', method:'GET', data: data }).then(function(results) { 
+			callTwitter($scope.method, JSON.parse($scope.params)).then(function(results) { 
 				sa(function() { 
 					$scope.resultsJSON = tojson(results);
 					$scope.results = results;
+					console.log(results);
 				});
 			}).fail(function(err) { 
 				console.error(err);
 			});
 		};
-		$timeout($scope.call, 1000);
+
+		$scope.byfriend = {};
+		var incr = function(id) { 
+			$scope.byfriend[id] = ($scope.byfriend[id] || 0) + 1;
+		};
+
+		var alls = [
+			callTwitter('direct_messages', {count:200}).then(function(results) {
+				sa(function() { results.map(function(x) { 
+					console.log(x);
+					incr(x.sender.screen_name); 
+				}); });
+			}),
+			callTwitter('statuses/user_timeline', {count:500}).then(function(results) {
+				console.info('user_timeline ', results);
+				sa(function() { 
+					results.map(function(x) { 
+						// once for the author:
+						incr(x.user.screen_name); 
+						// once for each @ mention
+						x.entities.user_mentions.map(function(mention) {
+							incr(mention.screen_name);
+						});
+					}); 
+				});				
+			})
+		];
+
+
+		window.s = $scope;
+
 	});
