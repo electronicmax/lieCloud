f31e0813d921303698114139c6c5ad327db88487
electronic <electronic@imac.home>
2015-07-27 00:57:58 +0100

    appj s

--- study/app.js
+++ study/app.js
@@ -59,7 +59,7 @@ app.post('/api/questionnaire',
 
 app.use(express.static('www'));
 
-var server = app.listen(8000, function () {
+var server = app.listen(80, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('Example app listening at http://%s:%s', host, port);
